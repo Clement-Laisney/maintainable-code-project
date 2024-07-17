27a0ae8b5ed1f7b4893b82589a2ad094f9a78d44
Clément Laisney <clement.laisney@outlook.fr>
2024-07-17 19:44:53 +0200

    ajout des armes et des personnages

---
+++ characters.py
@@ -0,0 +1,52 @@
+from weapons import WEAPONS
+
+CHARACTERS = {
+    "Ilona": {
+        "name": "Ilona",
+        "hit_points": 10,
+        "armor": 14,
+        "desc": "Barde, Demi-elfe",
+        "weapon1": WEAPONS["Dague"],
+        "weapon2": WEAPONS["Murmures Dissonants"],
+    },
+    "Urmora": {
+        "name": "Urmora",
+        "hit_points": 14,
+        "armor": 18,
+        "desc": "Guerrière, Naine",
+        "weapon1": WEAPONS["Marteau léger"],
+        "weapon2": WEAPONS["Arbalète"],
+    },
+    "Karad": {
+        "name": "Karad",
+        "hit_points": 9,
+        "armor": 16,
+        "desc": "Moine, Humain",
+        "weapon1": WEAPONS["Marteau léger"],
+        "weapon2": WEAPONS["Marteau de guerre"],
+    },
+    "Peren": {
+        "name": "Peren",
+        "hit_points": 8,
+        "armor": 13,
+        "desc": "Occultiste, Elf",
+        "weapon1": WEAPONS["Dague"],
+        "weapon2": WEAPONS["Décharge Occulte"],
+    },
+    "Aloïs": {
+        "name": "Aloïs",
+        "hit_points": 9,
+        "armor": 14,
+        "desc": "Roublard, Humain",
+        "weapon1": WEAPONS["Dague"],
+        "weapon2": WEAPONS["Rapière"],
+    },
+    "Hommet": {
+        "name": "Hommet",
+        "hit_points": 9,
+        "armor": 12,
+        "desc": "Magicien, Humain",
+        "weapon1": WEAPONS["Dague"],
+        "weapon2": WEAPONS["Contact Glacial"],
+    },
+}
---
+++ weapons.py
@@ -0,0 +1,50 @@
+WEAPONS = {
+    "Rapière": {
+        "name": "Rapière",
+        "damage": "1d8",
+        "category": "Heavy",
+        "desc": "C'est une belle rapière en bon état !",
+    },
+    "Dague": {
+        "name": "Dague",
+        "damage": "1d4",
+        "category": "Light",
+        "desc": "Cette dague m'a l'air de renfermer un secret !",
+    },
+    "Marteau de guerre": {
+        "name": "Marteau de guerre",
+        "damage": "1d8",
+        "category": "Heavy",
+        "desc": "Le sang des ennemies semble encore frais sur cette arme",
+    },
+    "Marteau léger": {
+        "name": "Marteau",
+        "damage": "1d6",
+        "category": "Light",
+        "desc": "Je suis un marteau !",
+    },
+    "Arbalète": {
+        "name": "Arbalète",
+        "damage": "1d8",
+        "category": "Light",
+        "desc": "Vise la tête !",
+    },
+    "Décharge Occulte": {
+        "name": "Décharge Occulte",
+        "damage": "1d10",
+        "category": "Magic",
+        "desc": "A qui sont ces os ?",
+    },
+    "Murmures Dissonants": {
+        "name": "Murmures Dissonants",
+        "damage": "3d6",
+        "category": "Magic",
+        "desc": "Qui me parle ?",
+    },
+    "Contact Glacial": {
+        "name": "Contact Glacial",
+        "damage": "1d8",
+        "category": "Magic",
+        "desc": "Ouhla... fait frisquet ici !",
+    },
+}
