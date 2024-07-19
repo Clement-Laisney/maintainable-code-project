32006fcbf5f9c876d80747bba3a90d62eff17c9c
Clément Laisney <clement.laisney@outlook.fr>
2024-07-19 16:29:23 +0200

    added docstring to controler.py

--- controler.py
+++ controler.py
@@ -1,5 +1,5 @@
 from random import choices, choice, shuffle, randint
-from model import Character, Monster
+from model import Character, Monster, Weapon
 from characters import CHARACTERS
 from monsters import MONSTERS
 
@@ -22,60 +22,87 @@ class Controller:
         self.view = view
 
     def get_players(self) -> None:
+        """Retrieve the player name and ask the player to choose his character."""
+
         while len(self.characters) < MAX_PLAYERS:
             player = self.view.prompt_for_player_name()
             if not player:
                 return None
-            character_abilities = self.view.prompt_for_character(
+
+            # prompt_for_character returns a dictionnary with character properties
+            character_properties = self.view.prompt_for_character(
                 player,
                 self.playable_characters,
             )
-            if not character_abilities:
+            if not character_properties:
                 return None
-            character = Character(**character_abilities)
+
+            character = Character(**character_properties)
             self.characters.append(character)
+
+            # delete the character from the list of available characters
             del self.playable_characters[character.name]
+
+            # map the name of the character to the player name
             self.players[character.name] = player
 
     def get_pnjs(self) -> None:
         while len(self.characters) + len(self.pnjs) < MAX_PLAYERS:
+            # attribute a number to a pnj (ie: first = 1 etc...)
             pnj_number = len(self.pnjs) + 1
-            character_abilities = self.view.prompt_for_character(
-                f"bot{pnj_number}",
+
+            # prompt_for_character returns a dictionnary with properties
+            character_properties = self.view.prompt_for_character(
+                f"bot #{pnj_number}",
                 self.playable_characters,
             )
-            if not character_abilities:
+            if not character_properties:
                 return None
-            character = Character(**character_abilities)
+
+            character = Character(**character_properties)
             self.pnjs.append(character)
+
+            # delete the character from the list of available characters
             del self.playable_characters[character.name]
-            self.players[character.name] = f"bot{pnj_number}"
+
+            # map the name of the character to the bot name
+            self.players[character.name] = f"bot #{pnj_number}"
 
     def get_difficulty(self) -> None:
+        """Retrieve the difficulty of the dungeon"""
         self.dungeon_difficulty = self.view.prompt_difficulty()
 
     def select_monsters(self) -> None:
+        """Choose and Create monster objects"""
+        # Adapt the number of monsters with the number of players
         monster_number = min(len(self.players) + 1, MAX_MONSTERS)
+
+        # Retrieve monsters with appropriate difficulty level
         available_monsters = [
             monster
             for monster in MONSTERS
             if (MONSTERS[monster]["difficulty"] <= self.dungeon_difficulty)
         ]
+
         choosed_monsters = choices(available_monsters, k=monster_number)
 
+        # Create monster instances
         for monster_name in choosed_monsters:
-            monster_abilities = MONSTERS[monster_name]
-            monster = Monster(**monster_abilities)
+            monster_properties = MONSTERS[monster_name]
+            monster = Monster(**monster_properties)
             self.monsters.append(monster)
 
     def roll_initiative(self) -> None:
+        """Draws the combat order"""
         self.queue = self.characters + self.pnjs + self.monsters
         shuffle(self.queue)
 
-    def start_dungeon(self):
+    def start_dungeon(self) -> None:
+        """Start the round per round combat while monsters or players/pnjs not dead"""
         is_running = True
         while is_running:
             for character in self.queue:
+                # If at least 1 monster and 1 player/pnj
                 if (len(self.characters) + len(self.pnjs) > 0) and len(
                     self.monsters
                 ) > 0:
@@ -105,6 +132,8 @@ class Controller:
                             damages = character.attack(target, weapon)
                             self.view.display_damages(character, target, damages)
                             is_still_alive = target.is_alive
+
+                            # If dead remove it from the list of alive monsters/players/pnj
                             if not is_still_alive:
                                 if target in self.characters:
                                     self.characters.remove(target)
@@ -118,23 +147,56 @@ class Controller:
                 else:
                     is_running = False
 
-    def choose_target(self, available_target):
+    def choose_target(self, available_target: list["Character"]) -> "Character":
+        """Draws a Monster or Player/PNJ to target
+
+        Args:
+            available_target (list[Character]): A list of possible objects to target
+
+        Returns:
+            Character: A Character object
+        """
         target = choice(available_target)
         return target
 
-    def choose_weapon(self, weapons):
+    def choose_weapon(self, weapons: list["Weapon"]) -> "Weapon":
+        """Draws a weapon to attack with
+
+        Args:
+            weapons (list[Weapon]): A list of possible weapons objects to choose
+
+        Returns:
+            Weapon: A Weapon object
+        """
         weapon = choice(weapons)
         return weapon
 
-    def roll_attack(self, target):
+    def roll_attack(self, target: "Character") -> bool:
+        """Rolls a 20-faced dice and compare it to the armor class of the target
+
+        Args:
+            target (Character): The Character object of the target
+
+        Returns:
+            bool: True if the dice result > target armor class
+        """
         armor = target.armor
         dice = randint(1, 21)
         return dice > armor
 
-    def winner(self, heroes, monsters):
+    def winner(self, heroes: list["Character"], monsters: list["Character"]) -> str:
+        """Get the winner
+
+        Args:
+            heroes (list[Character]): The list of Players and PNJ Character objects alive
+            monsters (list[Character]): The list of Monster objects alive
+
+        Returns:
+            str: "Monsters or "Heroes"
+        """
         if len(heroes) == 0:
             return "Monsters"
-        else:
+        elif len(monsters) == 0:
             return "Heroes"
 
     def run(self) -> None:
